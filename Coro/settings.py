6213e70f8f737eab614a1db79097964a12977298
Javier Sinuka <javier_sinuka@mi.unc.edu.ar>
2023-08-21 19:22:45 -0300

    Agregado de configuraciones generales con respecto a lo basico (Materialize, PostgreSQL, etc)

--- Coro/settings.py
+++ Coro/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Coro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_coro_pio',
+        'USER': 'javier_sinuka',
+        'PASSWORD': 'contrasenia',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -123,3 +127,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# PERSONAL
+
+
+
